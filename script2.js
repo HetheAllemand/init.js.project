ca39ff2e642ab74519727ca3fc45c326a4faf9c6
HetheAllemand <118325715+HetheAllemand@users.noreply.github.com>
2022-12-12 20:36:24 -0500

    ex num2
    ex 2

--- script2.js
+++ script2.js
@@ -1,1 +1,13 @@
 console.log("Script2");
+let userName= "Hethe";
+let email= "holler@fake.com";
+let userPassword= "pass123";
+let age= "27";
+let country= "Greece";
+let monthlySalary= 2000;
+console.log(`${userName} ${email} ${monthlySalary*12} (annual)`);
+document.write(`
+<p>User: ${userName} </p>
+<p>Email: ${email} </p>
+<p>Salary: ${monthlySalary*12} </p>
+`)
